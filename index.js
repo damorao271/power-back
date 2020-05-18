f3a40429f936c2341ae58cd3eaa8e809a96d263e
Daniel Morao <13-10932@usb.ve>
2020-05-18 12:45:59 -0400

    Index escuchando puerto y model user a medias

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.get;
+mongoose
+  .connect("mongodb://localhost/power")
+  .then(() => console.log("Conectado a MongoDB ..."))
+  .catch((err) => console.error("Could not connect to  MongoDB ..."));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
